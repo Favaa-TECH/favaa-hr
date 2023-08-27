1f033394e802a04a5e3827431eb345c81c5b5925
Muhammad Ali Faatikh Riziq <faatikhriziq@gmail.com>
2023-08-27 13:28:53 +0700

    feat: count notif badge in leave and permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Leave;
+use App\Models\Permission;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('components.layouts.partials.sidenav',function($view){
+            $view->with([
+                'leaveCount'=> Leave::all()->where('status','pending')->count(),
+                'permissionCount' => Permission::all()->where('status','pending')->count(),
+            ]);
+        });
     }
 }
